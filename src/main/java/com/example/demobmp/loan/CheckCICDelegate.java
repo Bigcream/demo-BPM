7c1929cbbafba558d41fe0a0cb14429e4c1a7e0c
bigcream <duyk63uet@gmail.com>
2025-03-07 22:23:31 +0700

    test security open source

--- src/main/java/com/example/demobmp/loan/CheckCICDelegate.java
+++ src/main/java/com/example/demobmp/loan/CheckCICDelegate.java
@@ -3,14 +3,15 @@ package com.example.demobmp.loan;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
-
+import java.security.SecureRandom;
 import java.util.Random;
 
 @Component
 public class CheckCICDelegate implements JavaDelegate {
     @Override
     public void execute(DelegateExecution execution) {
-        int cicScore = new Random().nextInt(900);  // Giả lập điểm CIC ngẫu nhiên
+//        int cicScore = new Random().nextInt(900);  // Giả lập điểm CIC ngẫu nhiên
+        int cicScore = new SecureRandom().nextInt();
         boolean approved = cicScore > 600;  // Điều kiện phê duyệt
         execution.setVariable("cicApproved", approved);
         System.out.println("Checked CIC Score: " + cicScore + " | Approved: " + approved);

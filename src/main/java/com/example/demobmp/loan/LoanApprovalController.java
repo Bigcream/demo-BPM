0b69fa06ac2d0e9feed9369cfe9ff2c60a6429ad
bigcream <duyk63uet@gmail.com>
2025-04-09 17:56:42 +0700

    test sonar

--- src/main/java/com/example/demobmp/loan/LoanApprovalController.java
+++ src/main/java/com/example/demobmp/loan/LoanApprovalController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 @RequestMapping("/loan")
 public class LoanApprovalController {
@@ -19,6 +21,8 @@ public class LoanApprovalController {
     @PostMapping("/apply")
     public ResponseEntity<String> startLoanApprovalProcess() {
         System.out.println("test");
+        int cicScore = new Random().nextInt(900);  // Giả lập điểm CIC ngẫu nhiên
+        System.out.println(cicScore);
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("loanApprovalProcess");
         return ResponseEntity.ok("Loan Approval Process Started: " + processInstance.getId());
     }
